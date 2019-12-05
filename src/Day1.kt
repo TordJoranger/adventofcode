d0cd91856a9077873b3e83e96d725becb67af76c
Tord Joranger <TORDJ@equinor.com>
2019-12-05 16:45:24 +0100

    day 1 and 3

---
+++ src/Day1.kt
@@ -0,0 +1,17 @@
+import java.io.File
+
+fun day1() = fuel(File("day1.txt").readLines().map { it.toInt() })
+
+  private fun calc(x : Int) : Int {
+        val y = x/3 - 2
+        return if(y > 8)
+            y + calc(y)
+        else
+            y
+    }
+
+  private fun fuel(list: List<Int>) : Int {
+        if(list.count() > 0)
+            return calc(list.first()) + fuel(list.drop(1))
+        return 0
+    }
---
+++ src/app.kt
@@ -0,0 +1,5 @@
+
+fun main() {
+    println(day1())
+    println(day3())
+}
