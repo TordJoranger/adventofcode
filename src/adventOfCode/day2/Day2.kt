23d3e8dd7a1f2d21df53cd4c1568897107e15f01
Tord Joranger <tord@norsefeedback.no>
2020-12-02 17:05:50 +0100

    clean up

--- src/adventOfCode/day2/Day2.kt
+++ src/adventOfCode/day2/Day2.kt
@@ -1,51 +1,22 @@
 package adventOfCode.day2
-
 import java.io.File
 
+fun day2() = countValidPasswords(File("src/adventOfCode/day2/day2.txt").readLines())
 
-fun day2() = password(File("src/adventOfCode/day2/day2.txt").readLines())
-
-private fun password(list: List<String>) : Int {
-    var valid = 0
-
-    list.forEach { pa ->
-       if(isValidPart2(pa))
-           valid++
-    }
-    return valid
+private fun countValidPasswords(list: List<String>) : Int  {
+   return  list.sumBy { s ->
+       val split = s.split(" ", "-", ": ")
+       val lowerBound = split[0].toInt() -1 //subtract to match index
+       val upperBound = split[1].toInt() -1 //subtract to match index
+       val letter = split[2][0] //assumes always 1 letter
+       val password = split[3]
+       isValidPart2(lowerBound,upperBound,letter,password) }
 }
 
-private fun isValidPart2(string : String) : Boolean {
-    val spaceSplit = string.split(" ")
-    val rangeSplit = spaceSplit[0].split("-")
-    val lowerBound = rangeSplit[0].toInt()
-    val upperBound = rangeSplit[1].toInt()
-
-    val letter = spaceSplit[1][0]
-
-    val password = spaceSplit[2]
-
-    if((password[lowerBound-1] == letter) xor (password[upperBound-1] == letter))
-        return true
-
-    return false
+private fun isValidPart1(lowerBound: Int,upperBound: Int,letter: Char,password : String) : Int {
+    val count = password.count{c -> c == letter}
+    return if (count in lowerBound..upperBound) 1 else 0
 }
-
-
-private fun isValidPart1(string : String) : Boolean {
-    val spaceSplit = string.split(" ")
-    val rangeSplit = spaceSplit[0].split("-")
-    val lowerBound = rangeSplit[0].toInt()
-    val upperBound = rangeSplit[1].toInt()
-
-    val letter = spaceSplit[1][0]
-
-    val password = spaceSplit[2]
-
-    val  count = password.count{a-> a == letter}
-
-    if(count in lowerBound..upperBound)
-        return true
-
-    return false
+private fun isValidPart2(lowerBound: Int,upperBound: Int,letter: Char,password : String) : Int {
+    return if ((password[lowerBound] == letter) xor (password[upperBound] == letter)) 1 else 0
 }
