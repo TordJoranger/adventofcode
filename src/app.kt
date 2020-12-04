0bbadb5905c176212356cd2d415b4a7a5089d7cd
Tord Joranger <tord@norsefeedback.no>
2020-12-04 16:35:53 +0100

    day 4

--- src/app.kt
+++ src/app.kt
@@ -2,7 +2,7 @@ import day6.day6part1
 import day6.day6part2
 
 fun main() {
-     println(adventOfCode.day3.day3())
+     println(adventOfCode.day4.day4Stolen())
 //     println(day3.txt.day3part1())
 //     println(day3.txt.day3part2())
     //println(day6part1())
