7b8515165eaeafbfd0adebe20cef7df5e75d9128
Tord at home <tord_joranger@hotmail.com>
2020-12-06 21:13:03 +0100

    day5

--- src/app.kt
+++ src/app.kt
@@ -1,8 +1,5 @@
-import day6.day6part1
-import day6.day6part2
-
 fun main() {
-     println(adventOfCode.day4.day4Stolen())
+     println(adventOfCode.day5.day5())
 //     println(day3.txt.day3part1())
 //     println(day3.txt.day3part2())
     //println(day6part1())
