435fbf573f4005af1bb74658d8355d7e3bc9a8df
Tord Joranger <tord@norsefeedback.no>
2020-12-10 16:23:22 +0100

    day9

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,5 @@
 fun main() {
-     println(adventOfCode.day8.day8())
+     println(adventOfCode.day9.day9())
 //     println(day3.txt.day3part1())
 //     println(day3.txt.day3part2())
     //println(day6part1())
