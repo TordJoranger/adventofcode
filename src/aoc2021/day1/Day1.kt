6bbc22c7f785c0b6c4fa24b271fbd8aefe4eeb0b
Tord Joranger <TORDJ@equinor.com>
2021-12-09 16:40:20 +0100

    up to day 4

---
+++ src/aoc2021/day1/Day1.kt
@@ -0,0 +1,43 @@
+package aoc2021.day1
+
+import java.io.File
+
+fun day1() = countIncrementsPart2(File("src/aoc2021/day1/day1.txt").readLines())
+
+fun countIncrements(list: List<String>): Int {
+
+    var count = 0
+
+    list.forEachIndexed { index, s ->
+        if (index != 0 && s.toInt() > list[index - 1].toInt())
+            count += 1
+    }
+
+
+    return count
+
+}
+
+fun countIncrementsPart2(list: List<String>): Int {
+
+    var count = 0
+    val ints = list.map { s-> s.toInt() };
+    val size = list.size
+
+   ints.forEachIndexed { index, s ->
+       if(index != 0 && index < size -2) {
+           val previous = ints[index+1] + s + ints[index-1]
+           val current = s+  ints[index+1] + ints[index+2]
+
+           if (current > previous)
+               count += 1
+       }
+
+
+    }
+
+
+    return count
+
+}
+
--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,5 @@
 fun main() {
-     println(adventOfCode.day10.day10())
+     println(aoc2021.day4.day4())
 //     println(day3.txt.day3part1())
 //     println(day3.txt.day3part2())
     //println(day6part1())
