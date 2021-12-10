96c03a0b48d47e0e1fcceb0167c9ff05e1d4c0a4
Tord Joranger <TORDJ@equinor.com>
2021-12-10 10:45:52 +0100

    day5

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,5 @@
 fun main() {
-     println(aoc2021.day4.day4())
+     println(aoc2021.day5.day5())
 //     println(day3.txt.day3part1())
 //     println(day3.txt.day3part2())
     //println(day6part1())
