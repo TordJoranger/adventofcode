8ed33f2f1a64389c67856c8516c32c6a4113e9fe
Tord Joranger <TORDJ@equinor.com>
2022-12-04 15:02:30 +0100

    day 4 count

--- src/main/kotlin/aoc2022/day4/Day4.kt
+++ src/main/kotlin/aoc2022/day4/Day4.kt
@@ -1,15 +1,15 @@
 package main.aoc2022.day4
 import java.io.File
 
-fun part1(input: File) : Int =input.readLines().sumOf(::getOverLapsP1)
-fun part2(input: File) : Int = input.readLines().sumOf(::getOverLapsP2)
+fun part1(input: File) : Int = getOverLapsP1(input.readLines())
+fun part2(input: File) : Int = getOverLapsP2(input.readLines())
 
-private fun getOverLapsP1(it: String) : Int {
-    val (a,b) = it.split(",").map{it.split("-").map(String::toInt) }
-    return if (a[0] <= b[0] && a[1] >= b[1] || b[0] <= a[0] && b[1] >= a[1]) 1 else 0
+private fun getOverLapsP1(list: List<String>) : Int = list.count{ l ->
+     val (a, b) = l.split(",").map { it.split("-").map(String::toInt) }
+     (a[0] <= b[0] && a[1] >= b[1] || b[0] <= a[0] && b[1] >= a[1])
 }
 
-private fun getOverLapsP2(it: String) : Int {
-    val (a,b) = it.split(",").map{it.split("-").map(String::toInt) }.map { it[0]..it[1] }
-    return if (a.intersect(b).isNotEmpty()) 1 else 0
-}
+private fun getOverLapsP2(list: List<String>) : Int = list.count { l ->
+     val (a, b) = l.split(",").map { it.split("-").map(String::toInt) }.map { it[0]..it[1] }
+     (a.intersect(b).isNotEmpty())
+ }
