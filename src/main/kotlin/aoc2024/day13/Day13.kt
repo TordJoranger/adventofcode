295a175309ce17f1d6407a9d01c51507321c40f5
tordj <tordj@equinor.com>
2024-12-13 14:07:54 +0100

    minor

--- src/main/kotlin/aoc2024/day13/Day13.kt
+++ src/main/kotlin/aoc2024/day13/Day13.kt
@@ -6,77 +6,58 @@ import java.io.File
 fun part1(input: File) : Long = findLeastPushes(input.readLines())
 
 fun findLeastPushes(readLines: List<String>): Long {
-
-    val windowned = readLines.windowed(3, step = 4)
-
-   val sum = windowned.sumOf { m ->
+   val sum = readLines.windowed(3, step = 4).sumOf { m ->
         val solveSubstitution = solveSubstitution(m)
        (solveSubstitution.first *3 + solveSubstitution.second).toLong()
     }
-
     return sum
 }
 
 fun solveSubstitution(m: List<String>) : Pair<Int,Int> {
     val regex = "\\d+".toRegex()
-    val line1 =  regex.findAll(m[0]).map { it.value.toInt() }
-    val line2 =  regex.findAll(m[1]).map { it.value.toInt() }
-    val line3 =  regex.findAll(m[2]).map { it.value.toInt() }
+    val line1 =  regex.findAll(m[0]).map { it.value.toDouble() }
+    val line2 =  regex.findAll(m[1]).map { it.value.toDouble() }
+    val line3 =  regex.findAll(m[2]).map { it.value.toDouble() }
+    val solutions = solveLinearEquations(
+        line1.first(),
+        line2.first(),
+        line3.first(),
+        line1.last(),
+        line2.last(),
+        line3.last())
 
-    val p = solveLinearEquations(
-        line1.first().toDouble(),
-        line2.first().toDouble(),
-        line3.first().toDouble(),
-        line1.last().toDouble(),
-        line2.last().toDouble(),
-        line3.last().toDouble()
-        )
-
-    if(p?.first?.rem(1.0)  == 0.0 && p.second.rem(1.0) ==0.0){
-        return Pair(p.first.toInt(),p.second.toInt())
-    }
-
-
-   return Pair(0,0)
-
-
-
-
+    return if (solutions?.first?.rem(1.0)  == 0.0 && solutions.second.rem(1.0) ==0.0) {
+        Pair(solutions.first.toInt(),solutions.second.toInt())
+    } else Pair(0,0)
 }
 
 fun part2(input: File) : Long = findLeastPushes2(input.readLines())
 
 fun findLeastPushes2(readLines: List<String>): Long {
-    val windowned = readLines.windowed(3, step = 4)
-    val sum = windowned.sumOf { m ->
+    val sum = readLines.windowed(3, step = 4).sumOf { m ->
         val solveSubstitution = solveSubstitution2(m)
-        (solveSubstitution.first.toLong() *3 + solveSubstitution.second)
+        (solveSubstitution.first *3 + solveSubstitution.second)
     }
-
     return sum
 }
 
 fun solveSubstitution2(m: List<String>) : Pair<Long,Long> {
     val regex = "\\d+".toRegex()
-    val line1 =  regex.findAll(m[0]).map { it.value.toInt() }
-    val line2 =  regex.findAll(m[1]).map { it.value.toInt() }
-    val line3 =  regex.findAll(m[2]).map { it.value.toInt() }
-
+    val line1 =  regex.findAll(m[0]).map { it.value.toDouble() }
+    val line2 =  regex.findAll(m[1]).map { it.value.toDouble() }
+    val line3 =  regex.findAll(m[2]).map { it.value.toDouble() }
     val p = solveLinearEquations(
-        line1.first().toDouble(),
-        line2.first().toDouble(),
-        line3.first().toDouble()+10000000000000,
-        line1.last().toDouble(),
-        line2.last().toDouble(),
-        line3.last().toDouble()+10000000000000
-    )
+        line1.first(),
+        line2.first(),
+        line3.first()+10000000000000,
+        line1.last(),
+        line2.last(),
+        line3.last()+10000000000000)
 
     if(p?.first?.rem(1.0)  == 0.0 && p.second.rem(1.0) ==0.0){
         return Pair(p.first.toLong(),p.second.toLong())
     }
-
     return Pair(0,0)
-
 }
 
 fun solveLinearEquations(
