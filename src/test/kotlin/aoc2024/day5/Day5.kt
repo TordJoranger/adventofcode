e420c0d6aec9f4c72064150ab52d969aeeacb508
tordj <tordj@equinor.com>
2024-12-16 10:18:48 +0100

    minor

--- src/test/kotlin/aoc2024/day5/Day5.kt
+++ src/test/kotlin/aoc2024/day5/Day5.kt
@@ -8,13 +8,13 @@ internal class Day5Test {
     private val file = File("src/test/kotlin/aoc2024/day5/test5.txt")
     @org.junit.jupiter.api.Test
     fun part1() {
-        val result = main.aoc2024.day6.day5.part1(file)
+        val result = main.aoc2024.day5.part1(file)
         Assertions.assertEquals(143, result)
     }
 
     @org.junit.jupiter.api.Test
     fun part2() {
-        val result = main.aoc2024.day6.day5.part2(file)
+        val result = main.aoc2024.day5.part2(file)
         Assertions.assertEquals(123, result)
     }
 }
